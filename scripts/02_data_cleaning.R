9932e51a22fa794969ccd5c628bb9c7efc36f203
julia-wrobel <julia.wrobel@cuanschutz.edu>
2024-07-18 16:24:34 -0400

    edits to project org

--- scripts/02_data_cleaning.R
+++ scripts/02_data_cleaning.R
@@ -1,21 +1,24 @@
-load(here::here("Data", "raw.Rdata"))
-
 # set up a variable to define which state you want to analyze
 state = "ga"
 
+# download raw data
+source(here::here("source", "01_data_download.R"))
+
 # grab only observations from the specified state
 covid = covid %>%
-  filter(grepl("ga", key_plot_id))
+  filter(grepl(state, key_plot_id))
 
 
 # only include columns from counties dataset we are interested in
-# wwtp_id is wastewater treatment plant id
-# county_fips is FIPS code for all counties served by this site
 counties = counties %>%
-  select(key_plot_id, wwtp_id, county = county_names, county_fips, population_served) %>%
+  filter(grepl(state, key_plot_id)) %>%
+  select(key_plot_id, wwtp_id, county = county_names,
+         county_fips, population_served) %>%
   distinct()
 
 # merge covid data with the county label information
+# convert  variables from character to numeric
+# concentration variable more intuitive name
 covid = left_join(covid, counties, by = "key_plot_id") %>%
   select(-key_plot_id) %>%
   mutate(pcr_conc_lin = as.numeric(pcr_conc_lin),
@@ -24,4 +27,4 @@ covid = left_join(covid, counties, by = "key_plot_id") %>%
 
 
 ## save intermediate data object and data data was accessed
-save(covid, file = here::here("Data", "clean.Rdata"))
+save(covid, file = here::here("data", "clean.Rdata"))
